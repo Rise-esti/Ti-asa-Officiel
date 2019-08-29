dc8ef26034b5cc75159ea45d8387383cff3fa6bd
Landry Manankoraisina RASENDRANIRINA <landry.manankoraisina.rasendranirina@esti.mg>
2019-08-29 14:31:45 +0300

    finition de footer.php

---
+++ footer.php
@@ -0,0 +1,31 @@
+	<link rel="icon" type="image/png" href="public/images/icons/favicon.ico"/>
+	<link rel="stylesheet" type="text/css" href="public/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="public/css/main2.css">
+	<link rel="stylesheet" href="public/assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="public/assets/css/style.css">
+	<link rel="stylesheet" href="public/bootstrap.min.css">
+		
+		<body>
+			<footer class="layout-dark">
+				<div class="copy-right">
+					<div class="container padding-tb-50px">
+						<div class="row">
+							<div class="col-lg-6">
+								<div class="copy-right-text text-lg-left text-center sm-mb-15px"><a target="_blank" href="#"><b>Ti-asa service pro</b></a> </div>
+							</div>
+							<div class="col-lg-6">
+								<!--  Social -->
+								<ul class="social-media list-inline text-lg-right text-center margin-0px text-white">
+									<li class="list-inline-item"><a class="facebook" href="https://www.facebook.com/Ti-asa-114419079928260"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li class="list-inline-item"><a class="linkedin" href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+									<li class="list-inline-item"><a class="google" href="#"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+									<li class="list-inline-item"><a class="twitter" href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+								</ul>
+								<!-- // Social -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</body>
+                                                     
