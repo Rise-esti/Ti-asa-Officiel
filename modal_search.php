b79ca7ed9508d5d929702771094e27548a1369ad
Landry Manankoraisina RASENDRANIRINA <landry.manankoraisina.rasendranirina@esti.mg>
2019-08-29 12:44:56 +0300

    modal_search

---
+++ modal_search.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Bienvenue sur ti-asa</title>
+<link rel="stylesheet" href="public/material-icon.css">
+<link rel="stylesheet" href="public/font-awesome.min.css">
+<link rel="stylesheet" href="public/bootstrap.min.css">
+<link rel="stylesheet" href="public/topnav.css">
+
+<link rel="stylesheet" href="public/css/bootstrap.min.css">
+<link rel="stylesheet" href="public/css/stylee.css">
+
+
+<script src="public/jquery.min.js"></script>
+<script src="public/bootstrap.min.js"></script>
+
+<!--===============================================================================================-->
+<link rel="icon" type="image/png" href="public/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="public/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="public/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="public/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="public/vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="public/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="public/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="public/vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="public/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="public/css/util.css">
+	<link rel="stylesheet" type="text/css" href="public/css/main2.css">
+<!--===============================================================================================-->
+
+<link rel="stylesheet" type="text/css" href="public/modal.css">
+
+<link rel="stylesheet" href="public/assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="public/assets/css/style.css">
+<link rel="stylesheet" href="public/assets/fonts/font-awesome/css/font-awesome.min.css">
+
+<link rel="stylesheet" href="public/bootstrap.min.css">
+<style type="text/css">
+	
+</style>
+
+	
+	<div class="modal-dialog modal-login" style="width:40% !important;" >
+		<div class="modal-content">
+			<div class="modal-header">				
+				<h4 class="modal-title" style="font-weight: bold;">S'inscrire</h4>
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+			</div>
+			<div class="modal-body">
+				<form  action="index.php?action=inscription" method="post">
+					<div class="form-group">
+						<input type="text" class="form-control" style="border-right: none !important; border-left: none !important; border-top:none !important; font-family: Poppins-Regular !important; font-size: 14px !important;" name="nom" placeholder="Nom" required="required">
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" style="border-right: none !important; border-left: none !important; border-top:none !important; font-family: Poppins-Regular !important; font-size: 14px !important;" name="prenom" placeholder="Prénom" required="required">
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" style="border-right: none !important; border-left: none !important; border-top:none !important; font-family: Poppins-Regular !important; font-size: 14px !important;" name="mail" placeholder="Email" required="required">
+					</div>
+					<div class="form-group">
+						<input type="password" class="form-control" style="border-right: none !important; border-left: none !important; border-top:none !important; font-family: Poppins-Regular !important; font-size: 14px !important;" name="password" placeholder="Mot de passe" required="required">
+					</div>
+					<div class="form-group">
+						<input type="password" class="form-control" style="border-right: none !important; border-left: none !important; border-top:none !important; font-family: Poppins-Regular !important; font-size: 14px !important;" name="confirmation_password" placeholder="Confirmer le mot de passe" required="required">
+					</div>
+					<div class="form-group">
+						<input type="submit" name="valider_inscription"  style="border-radius: 25px !important;" class="btn btn-primary btn-block btn-lg" value="S'inscrire">
+					</div>
+				</form>				
+				
+			</div>
+		</div>
+	</div>
+</div>     
+</body>
+</html>                                                        
