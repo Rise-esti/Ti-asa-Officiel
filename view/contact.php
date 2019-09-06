17772615b7f71e15d4cc00edc1a43753ec850a7d
Landry Manankoraisina RASENDRANIRINA <landry.manankoraisina.rasendranirina@esti.mg>
2019-09-06 14:01:07 +0300

    debug contact.php

--- view/contact.php
+++ view/contact.php
@@ -4,34 +4,26 @@
 	<title>Contact</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->
 	<link rel="icon" type="image/png" href="public/images/icons/favicon.ico"/>
-<!--===============================================================================================-->
+	
 	<link rel="stylesheet" type="text/css" href="public/vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="public/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="public/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="public/vendor/animate/animate.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="public/vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="public/vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="public/vendor/select2/select2.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="public/vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="public/css/util.css">
 	<link rel="stylesheet" type="text/css" href="public/css/main2.css">
-<!--===============================================================================================-->
+	
 	<link rel="stylesheet" href="public/material-icon.css">
-<link rel="stylesheet" href="public/font-awesome.min.css">
-<link rel="stylesheet" href="public/bootstrap.min.css">
-<link rel="stylesheet" href="public/topnav.css">
-<script src="public/jquery.min.js"></script>
-<script src="public/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="public/font-awesome.min.css">
+	<link rel="stylesheet" href="public/bootstrap.min.css">
+	<link rel="stylesheet" href="public/topnav.css">
+	
+	<script src="public/jquery.min.js"></script>
+	<script src="public/bootstrap.min.js"></script>
 
 	<link rel="stylesheet" href="public/assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="public/assets/css/style.css">
@@ -161,30 +153,6 @@
 	</div>
 
 
-
-	<div id="dropDownSelect1"></div>
-
-	<footer class="layout-dark">
-		<div class="copy-right">
-			<div class="container padding-tb-50px">
-				<div class="row">
-					<div class="col-lg-6">
-						<div class="copy-right-text text-lg-left text-center sm-mb-15px"><a target="_blank" href="#"><b>Ti-asa service pro</b></a> </div>
-					</div>
-					<div class="col-lg-6">
-						<!--  Social -->
-						<ul class="social-media list-inline text-lg-right text-center margin-0px text-white">
-							<li class="list-inline-item"><a class="facebook" href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-							<li class="list-inline-item"><a class="linkedin" href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-							<li class="list-inline-item"><a class="google" href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
-							<li class="list-inline-item"><a class="twitter" href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-						</ul>
-						<!-- // Social -->
-					</div>
-				</div>
-			</div>
-		</div>
-	</footer>
 
 <!--===============================================================================================-->
 	<script src="public/vendor/jquery/jquery-3.2.1.min.js"></script>
