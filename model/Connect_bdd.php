b8b334d9918e50932a6ea1b55292751074917ad5
Juliano Josoa <josoa.juliano.rahajaharimanga@esti.mg>
2019-09-06 19:34:57 +0300

    Modification majeure : Ajout token dans le backend

--- model/Connect_bdd.php
+++ model/Connect_bdd.php
@@ -1,7 +1,7 @@
 <?php
 class Connect_bdd{
     protected function dbconnect(){
-        $bdd = new PDO('mysql:host=localhost;dbname=tia_asa','sserver','sserver') or die('not connect');
+        $bdd = new PDO('mysql:host=localhost;dbname=tia_asa','tia-asa','tia-asa') or die('not connect');
         return $bdd;
     }
 }
