bc609575dd3a3f02fcbd3d9fe07ed6892f003ed4
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2019-09-07 23:55:37 +0300

    quelque correction interne

--- view/topnav.php
+++ view/topnav.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Ti-asa | HAcceuil </title>
+    <title>Ti-asa | Acceuil </title>
 
     <link rel="stylesheet" href="public/CSS/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
